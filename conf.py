1869e9a518378184a4f63514b83229bfcbc445ff
uberdeveloper <uberdeveloper001@gmail.com>
2021-11-06 15:14:17 +0530

    config changed to use cdn

--- conf.py
+++ conf.py
@@ -1248,7 +1248,7 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Bootstrap is served from BootstrapCDN (provided by MaxCDN)
 # Set this to False if you want to host your site without requiring access to
 # external resources.
-# USE_CDN = False
+USE_CDN = True 
 
 # Check for USE_CDN compatibility.
 # If you are using custom themes, have configured the CSS properly and are
